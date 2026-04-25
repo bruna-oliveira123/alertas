bbef95b9b568fa277b9569dbc0c28efcfa7077b6
bruna-oliveira123 <brunaom.ti@gmail.com>
2026-04-25 18:07:51 -0300

    Update soma.test.js

--- soma.test.js
+++ soma.test.js
@@ -1,20 +1,21 @@
-name: CI Alertas e Testes
-on: [push, pull_request]
+const soma = require('./soma');
 
-jobs:
-  testes:
-    runs-on: ubuntu-latest
-    steps:
-      - uses: actions/checkout@v4
-      - uses: actions/setup-node@v4
-        with:
-          node-version: '20'
-      - run: npm install
-      - run: npm test
-      - name: Notificar Discord
-        if: always()
-        run: |
-          curl -H "Content-Type: application/json" \
-          -X POST \
-          -d "{\"content\": \"Projeto Alertas: Os testes terminaram com status ${{ job.status }}\"}" \
-          ${{ secrets.DISCORD_WEBHOOK }}
+test('1. Soma de dois números positivos', () => {
+  expect(soma(1, 2)).toBe(3);
+});
+
+test('2. Soma de números negativos', () => {
+  expect(soma(-1, -1)).toBe(-2);
+});
+
+test('3. Soma com zero', () => {
+  expect(soma(5, 0)).toBe(5);
+});
+
+test('4. Resultado não deve ser nulo', () => {
+  expect(soma(2, 2)).not.toBeNull();
+});
+
+test('5. Soma de números grandes', () => {
+  expect(soma(100, 200)).toBe(300);
+});
