9aad1176191e52730e52511d6b7eed4b5591d73c
bruna-oliveira123 <brunaom.ti@gmail.com>
2026-04-25 17:12:54 -0300

    Create soma.test.js

---
+++ soma.test.js
@@ -0,0 +1,21 @@
+const soma = (a, b) => a + b;
+
+test('1. Soma de dois números positivos', () => {
+  expect(soma(1, 2)).toBe(3);
+});
+
+test('2. Soma de números negativos', () => {
+  expect(soma(-1, -1)).toBe(-2);
+});
+
+test('3. Soma com zero', () => {
+  expect(soma(5, 0)).toBe(5);
+});
+
+test('4. Resultado não deve ser nulo', () => {
+  expect(soma(2, 2)).not.toBeNull();
+});
+
+test('5. Soma de números grandes', () => {
+  expect(soma(100, 200)).toBe(300);
+});
